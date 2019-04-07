ca19ae48047bc7cddafddb49178209966ee4b99c
Katarzyna Paszko <kpaszko@spyro-soft.com>
2019-04-07 19:09:54 +0200

    sprawdzenie gita

--- src/scenes/MorseCode/components/services.js
+++ src/scenes/MorseCode/components/services.js
@@ -2,6 +2,8 @@ import alphabetInMorse from './const';
 
 export function morseCodeDecoder() {
     console.log(alphabetInMorse);
+    console.log("sprawdzenie gita");
+    
 }
 
 // module.exports = {morseCodeDecoder};
